d21d526c6a6d9d64daf86c6fb6de3840f9758f24
Azer Lahmar <azer.lahmar@esprit.tn>
2023-10-16 13:12:59 +0200

    last push

--- MsClaim/src/main/java/tn/esprit/msclaim/MsClaimApplication.java
+++ MsClaim/src/main/java/tn/esprit/msclaim/MsClaimApplication.java
@@ -2,7 +2,6 @@ package tn.esprit.msclaim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MsClaimApplication {
 
--- PaymentMs/src/main/java/com/example/paymentms/PaymentMsApplication.java
+++ PaymentMs/src/main/java/com/example/paymentms/PaymentMsApplication.java
@@ -1,15 +1,16 @@
-package com.esprit.developpementbusiness;
+package com.example.paymentms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class DeveloppementBusinessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeveloppementBusinessApplication.class, args);
-	}
+@EnableDiscoveryClient
+public class PaymentMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMsApplication.class, args);
+    }
 
 }
