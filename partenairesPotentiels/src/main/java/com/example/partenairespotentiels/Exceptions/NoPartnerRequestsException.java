f070638f3c702ecc11de1d239d076a8d8c574419
Jebriahmed <ahmed.jebri@esprit.tn>
2023-10-21 12:37:45 +0100

    partnership management+data base update+api gateway integration

---
+++ partenairesPotentiels/src/main/java/com/example/partenairespotentiels/Exceptions/NoPartnerRequestsException.java
@@ -0,0 +1,8 @@
+package com.example.partenairespotentiels.Exceptions;
+
+public class NoPartnerRequestsException extends RuntimeException {
+    public NoPartnerRequestsException(String message) {
+        super(message);
+    }
+}
+
